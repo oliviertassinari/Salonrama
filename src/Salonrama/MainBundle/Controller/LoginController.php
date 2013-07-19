394e3cd1c42c690976574d88da47ce9ce00bf8a3
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-07-20 00:36:32 +0200

    login mid

--- src/Salonrama/MainBundle/Controller/LoginController.php
+++ src/Salonrama/MainBundle/Controller/LoginController.php
@@ -3,17 +3,49 @@
 namespace Salonrama\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class LoginController extends Controller
 {
     public function loginAction()
     {
-    	$request = $this->get('request');
+    	$request = $this->getRequest();
 
     	if($request->isMethod('POST'))
     	{
-    		$email = htmlspecialchars(strtolower(trim($request->request->get('email', ''))));
-    		$password = trim($request->request->get('password', ''));
+            $session = $request->getSession();
+
+            if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+            {
+                $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+            }
+            else{
+                $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+                $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            }
+
+            echo $session->get(SecurityContext::LAST_USERNAME);
+
+            //$email = htmlspecialchars(strtolower(trim($request->request->get('email', ''))));
+    		//$password = trim($request->request->get('password', ''));
+
+            $form = $this->createFormBuilder(null)
+                    ->add('email', 'email', array('required' => true))
+                    ->add('password', 'password', array('required' => true))
+                    ->getForm();
+
+            $form->bind($request);
+
+            if($form->isValid())
+            {
+                echo '1';
+                $data = $form->getData();
+
+                print_r($data);
+            }
+            else{
+                echo '0';
+            }
     	}
 
         return $this->render('SalonramaMainBundle:Main:login.html.twig');
--- src/Salonrama/MainBundle/Entity/Salon.php
+++ src/Salonrama/MainBundle/Entity/Salon.php
@@ -13,8 +13,6 @@ use Doctrine\ORM\Mapping as ORM;
 class Salon
 {
     /**
-     * @var integer
-     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -28,23 +26,11 @@ class Salon
      */
     private $name;
 
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Salon
-     */
     public function setName($name)
     {
         $this->name = $name;
@@ -52,11 +38,6 @@ class Salon
         return $this;
     }
 
-    /**
-     * Get name
-     *
-     * @return string 
-     */
     public function getName()
     {
         return $this->name;
