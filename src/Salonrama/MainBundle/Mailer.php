20705b39e758185b89e8b6e7477d23925e7cf1a1
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-09-10 11:27:11 +0200

    custom error page

--- src/Salonrama/MainBundle/Mailer.php
+++ src/Salonrama/MainBundle/Mailer.php
@@ -15,7 +15,7 @@ class Mailer
 
     public function sendContact($email, $nom, $objet, $sujet, $message)
     {
-    	return $this->send('[Formulaire] '.$objet.' : '.$sujet, 'contact@salonrama.fr' ,'[Formulaire] '.$objet.' : '.$sujet, 'De : '.$nom.' ('.$email.')<br><br>'.$message);
+    	return $this->send('[Formulaire] '.$objet.' : '.$sujet, 'contact@salonrama.fr' ,'[Formulaire] '.$objet.' : '.$sujet, 'De : '.$nom.' ('.$email.')<br><br>Message :<br>'.$message);
     }
 
     public function sendForgotPassword($to, $name, $link)
