17fa9152dfae37a6b6ec4130cdf155abc7595627
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-10-09 20:38:17 +0200

    add blog articles recents

--- src/Salonrama/MainBundle/Controller/Account/IndexController.php
+++ src/Salonrama/MainBundle/Controller/Account/IndexController.php
@@ -9,23 +9,51 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-    	$session = $this->getRequest()->getSession();
+        $session = $this->getRequest()->getSession();
 
-    	$message = array();
+        $message = array();
 
-		foreach ($session->getFlashBag()->get('message', array()) as $value) {
-			$message = $value;
-		}
+        foreach ($session->getFlashBag()->get('message', array()) as $value) {
+            $message = $value;
+        }
 
-		$site = $this->getUser()->getAccount()->getSite();
+        $site = $this->getUser()->getAccount()->getSite();
+
+        include('/actualite/wp-load.php');
+
+        global $wp_query;
+
+        $wp_query = new \WP_Query('showposts=5&cat=4');
+        $blogAdvice = array();
+
+        while($wp_query->have_posts())
+        {
+            $wp_query->the_post();
+            $post = get_post();
+
+            $blogAdvice[] = array('link' => get_permalink($post->ID), 'title' => get_the_title($post->ID));
+        }
+
+        $wp_query = new \WP_Query('showposts=5');
+        $blogRecent = array();
+
+        while($wp_query->have_posts())
+        {
+            $wp_query->the_post();
+            $post = get_post();
+
+            $blogRecent[] = array('link' => get_permalink($post->ID), 'title' => get_the_title($post->ID));
+        }
 
         return $this->render('SalonramaMainBundle:Account:index.html.twig', array(
-        																		'message' => $message,
-        																		'snapshotUrl' => $site->getSnapshotUrl(),
+                                                                                'message' => $message,
+                                                                                'snapshotUrl' => $site->getSnapshotUrl(),
                                                                                 'siteUrl' => $site->getUrl(),
                                                                                 'last_update_interval' => Date::formatDateDiff($site->getLastUpdate()),
-                                                                                'creation_interval' => Date::formatDateDiff($site->getCreation())
-        																	));
+                                                                                'creation_interval' => Date::formatDateDiff($site->getCreation()),
+                                                                                'blog_advice' => $blogAdvice,
+                                                                                'blog_recent' => $blogRecent
+                                                                            ));
     }
 }
 
