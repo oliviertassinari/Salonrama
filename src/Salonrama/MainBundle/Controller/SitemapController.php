222f152e3dcf69583345687488f5c9f82cac9075
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-10-10 19:20:46 +0200

    sitemap : done

--- src/Salonrama/MainBundle/Controller/SitemapController.php
+++ src/Salonrama/MainBundle/Controller/SitemapController.php
@@ -15,21 +15,21 @@ class SitemapController extends Controller
 
     	$hostname = 'http://'.$this->getRequest()->getHost();
 
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_index'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_contact'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_presentation'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_forgot_password'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_presse'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_index'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_about'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_legal'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_policy'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_buildsite_step1'));
-    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_help'));
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_index'), 'priority' => 1);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_contact'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_presentation'), 'priority' => 0.6, 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_forgot_password'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_presse'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_index'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_about'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_legal'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_policy'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_buildsite_step1'), 'priority' => 0.6);
+    	$url[] = array('loc' => $hostname.$this->generateUrl('salonrama_main_help'), 'priority' => 0.6);
 
 		foreach ($siteList as $site)
 		{
-			$url[] = array('loc' => $site->getUrl(), 'lastmod' => $site->getLastUpdate());
+			$url[] = array('loc' => $site->getUrl(), 'lastmod' => $site->getLastUpdate(), 'priority' => 0.7);
 		}
 
         $response = $this->render('SalonramaMainBundle:Main:sitemap.xml.twig', array('url' => $url));
